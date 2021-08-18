9627f9e8b428ecf2908f8910203f959887f500af
mateo gonzalez <88322586+teorack@users.noreply.github.com>
2021-08-18 12:08:49 -0300

    gtadr

--- js/login.js
+++ js/login.js
@@ -33,4 +33,5 @@ function cargarErrores(id, idMensaje, mensaje) {
         // The ID token you need to pass to your backend:
         var id_token = googleUser.getAuthResponse().id_token;
         console.log("ID Token: " + id_token);
+        location.href = "index.html"
       }
