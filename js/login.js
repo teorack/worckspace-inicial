78273290246c9e8fb763715185f1e5c87a7f62da
mateo gonzalez <88322586+teorack@users.noreply.github.com>
2021-08-20 19:05:32 -0300

    zX

--- js/login.js
+++ js/login.js
@@ -6,16 +6,16 @@ document.addEventListener("DOMContentLoaded", function(e){
 });
 
     function onSignIn(googleUser) {
-        // Useful data for your client-side scripts:
+        // googole formlario
         var profile = googleUser.getBasicProfile();
-        console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+        console.log("ID: " + profile.getId());
         console.log('Full Name: ' + profile.getName());
         console.log('Given Name: ' + profile.getGivenName());
         console.log('Family Name: ' + profile.getFamilyName());
         console.log("Image URL: " + profile.getImageUrl());
         localStorage.setItem('email', profile.getEmail());
         location.href = "index.html"
-        // The ID token you need to pass to your backend:
+        
         var id_token = googleUser.getAuthResponse().id_token;
         console.log("ID Token: " + id_token);
         location.href = 'index.html'
@@ -25,8 +25,7 @@ document.addEventListener("DOMContentLoaded", function(e){
       function guardar(email, password){  
 
         if (email.trim()==="" || password.trim()===""){ //Chequea que el dato recibido no esté vacío. 
-        //El método trim elimina los espacios en blanco al inicio y al final del mismo.
-           spam ("El email está vacío");
+         
         }    else{
         localStorage.setItem("usuario", email.trim()); //setItem almacena el dato en la posición "usuario"
         localStorage.setItem("password", password.trim()); // Almaceno la contraseña
